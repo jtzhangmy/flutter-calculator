e4b795139097534a09766d10e737e85b01014e08
zhangmingyang <zhangmingyang@sogou-inc.com>
2019-07-11 11:02:04 +0800

    完成布局

--- lib/main.dart
+++ lib/main.dart
@@ -6,61 +6,327 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'welcome to flutter app',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('welcome to flutter'),
-        ),
-        body: new Center(
-          child: new Container(
+        title: 'welcome to flutter app',
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('welcome to flutter'),
+          ),
+          body: new Container(
             child: new Column(
               children: <Widget>[
-                new Row(
-                  children: <Widget>[
-                    new Container(
-                      child: new Center(
-                        child: new Text('a1'),
-                      ),
-                      width: 50,
-                      height: 50,
-                      decoration: new BoxDecoration(
-                        border: new Border.all(
-                          color: Color(0xFFFF0000),
-                          width: 1
-                        )
-                      ),
-                    ),
-                    new Text('a2'),
-                    new Text('a3')
-                  ],
+                new Container(
+                  child: new Text(
+                    '12345',
+                    style: new TextStyle(
+//                      color: Colors.white,
+                        fontSize: 24),
+                  ),
+                  width: 375,
+                  height: 150,
+                  padding: new EdgeInsets.all(20),
+                  decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.blue, width: 1)),
                 ),
-                new Row(
-                  children: <Widget>[
-                    new Text('b1'),
-                    new Text('b2'),
-                    new Text('b3')
-                  ],
+                new Container(
+                  child: new Column(
+                    children: <Widget>[
+                      new Row(
+                        children: <Widget>[
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                'C',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '+/-',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '%',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '/',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                        ],
+                      ),
+                      new Row(
+                        children: <Widget>[
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '7',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '8',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '9',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                'x',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                        ],
+                      ),
+                      new Row(
+                        children: <Widget>[
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '4',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '5',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '6',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '-',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                        ],
+                      ),
+                      new Row(
+                        children: <Widget>[
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '1',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '2',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '3',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                          new Container(
+                            child: new Center(
+                              child: new Text(
+                                '+',
+                                style: new TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            width: 68,
+                            height: 68,
+                            margin: new EdgeInsets.all(10),
+                            decoration: new BoxDecoration(
+                                /*border: new Border.all(
+                              color: Color(0xFFFF0000), width: 1),*/
+                                borderRadius: new BorderRadius.all(
+                                    const Radius.circular(40)),
+                                color: Colors.black12),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  margin: new EdgeInsets.all(10),
                 ),
-                new Row(
-                  children: <Widget>[
-                    new Text('c1'),
-                    new Text('c2'),
-                    new Text('c3')
-                  ],
-                )
               ],
             ),
-            width: 320,
-            height: 320,
-            decoration: new BoxDecoration(
-              border: new Border.all(
-                color: Colors.red,
-                width: 1
-              )
-            )
+            width: 375,
+            height: 812,
           ),
-        ),
-      )
-    );
+        ));
   }
 }
